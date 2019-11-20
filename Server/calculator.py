01f0dc62429b0230bdfd3b241e801b458491e4dd
Jonathan David <jday.david.2002@gmail.com>
2019-11-20 19:04:15 +0200

    Changed the name of the function and removed prints.

--- Server/calculator.py
+++ Server/calculator.py
@@ -3,7 +3,7 @@ from py_expression_eval import Parser
 ONE_PARAM_OPS = ['log','cos','sin','tan','abs','asin','acos','atan']
 
 
-def run(num1,op,num2,base=10):
+def calculate(num1,op,num2,base=10):
     """
     This function will calculate the result of the mathematical expression
     :param num1: the first number in the calculation (float or int)
@@ -27,9 +27,3 @@ def run(num1,op,num2,base=10):
         res =  parser.parse(str(num1) + " " + op + str(num2))
     # Currently, does not support equations.
     return res.evaluate({})
-
-
-# print(run(1.7,'+',33.15))
-# print(run(1,'^',15))
-# pri(nt(run(-15,'log',13, base=40))
-print(run(15,'sin',None))
