046731bfc2ada57645e49b617e2f475c65fed07d
Jonathan David <jday.david.2002@gmail.com>
2019-11-28 22:59:02 +0200

    Addressed merge request issues
    Added comment for base parameter, added default value to num2, change to using isinstance() instead of checking types directly.
    Also, added '' to the one param operators (because then it will just give the value of num1 (it's useful when checking the values of constants such as Pi).

--- Server/calculator.py
+++ Server/calculator.py
@@ -1,17 +1,18 @@
 from py_expression_eval import Parser
 
-ONE_PARAM_OPS = ['log','cos','sin','tan','abs','asin','acos','atan']
+ONE_PARAM_OPS = ['','log','cos','sin','tan','abs','asin','acos','atan']
 
 
-def calculate(num1,op,num2,base=10):
+def calculate(num1,op,num2=0,base=10):
     """
     This function will calculate the result of the mathematical expression
     :param num1: the first number in the calculation (float or int)
     :param op: the operation to perform on the two numbers (for example, '+') (str)
     :param num2: the second number in the calculation (float or int)
+    :param base: the base to use in the log operation (float or int, only relevant in log operations)
     :return: the result of the calculation (float or int)
     """
-    if type(num1) not in [int,float] or type(num2) not in [int,float] or type(op) != str:
+    if not isinstance(num1,(int,float)) or not (isinstance(num2,(int,float)) or num2 is None) or not isinstance(op,(str,)):
         raise TypeError("Can't perform operation (Invalid types)!")
     parser = Parser() 
     exp = ""
@@ -27,4 +28,3 @@ def calculate(num1,op,num2,base=10):
         exp =  f"{num1} {op} {num2}"
     # Currently, does not support equations.
     return parser.parse(exp).evaluate({})
-
