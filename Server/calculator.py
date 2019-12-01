6cc9cb2cac4023b8a702019c6f7b78cc1061c9a2
Jonathan David <jday.david.2002@gmail.com>
2019-12-01 19:30:10 +0200

    Optimized code and stopped checking input
    Changed if-else to elifs, changed the way the amount of parameters is checked, changed the default value for num2

--- Server/calculator.py
+++ Server/calculator.py
@@ -1,9 +1,6 @@
 from py_expression_eval import Parser
 
-ONE_PARAM_OPS = ['','log','cos','sin','tan','abs','asin','acos','atan']
-
-
-def calculate(num1,op,num2=0,base=10):
+def calculate(num1,op,num2=None,base=10):
     """
     This function will calculate the result of the mathematical expression
     :param num1: the first number in the calculation (float or int)
@@ -12,19 +9,17 @@ def calculate(num1,op,num2=0,base=10):
     :param base: the base to use in the log operation (float or int, only relevant in log operations)
     :return: the result of the calculation (float or int)
     """
-    if not isinstance(num1,(int,float)) or not (isinstance(num2,(int,float)) or num2 is None) or not isinstance(op,(str,)):
-        raise TypeError("Can't perform operation (Invalid types)!")
     parser = Parser() 
     exp = ""
-    if op in ONE_PARAM_OPS:
-        # Ignore the second number and use the base instead
-        if op == 'log':
-            exp = f"{op}({num1},{base})"
-        else:
-            exp = f"{op}({num1})"
-    else:
-        if num2 == None:
-            raise TypeError("Too few arguments!")
+    # Only one parameter was given and the one parameter operations need to be used
+    if num2:
         exp =  f"{num1} {op} {num2}"
+    # Ignore the second number and use the base instead
+    elif op == 'log':
+            exp = f"{op}({num1},{base})"
+    else:
+        exp = f"{op}({num1})"
+        
     # Currently, does not support equations.
     return parser.parse(exp).evaluate({})
+
