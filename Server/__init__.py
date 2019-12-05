0f9f899c766054ebdc81dde2473ef2c2a991f42d
itay shirizly <it.shirizly@gmail.com>
2019-12-05 12:50:25 +0200

    Changed the error messages to be in an enum

--- Server/__init__.py
+++ Server/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from enum import Enum
+
+class ProtocolErrors(Enum):
+    INVALID_PARAMETERS_ERROR, USER_ALREADY_LOGGED_ERROR, INVALID_CREDENTIALS_ERROR, USER_NOT_LOGGED_ERROR, PARAMETERS_DOES_NOT_MATCH_REQUIREMENTS = range(5)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
--- Server/routes.py
+++ Server/routes.py
@@ -1,45 +1,40 @@
-from Server import app, db, bcrypt
+from Server import app, db, bcrypt, ProtocolErrors
 from flask import request, jsonify
 from Server.models import User
 from flask_login import login_user, current_user, logout_user, login_required
 from Server.validators import validate_username, validate_email, validate_password
 
-INVALID_PARAMETERS_ERROR = "Invalid parameters were given"
-USER_ALREADY_LOGGED_ERROR = "User is already logged in"
-INVALID_CREDENTIALS_ERROR = "Email, Username, or Password is invalid"
-USER_NOT_LOGGED_ERROR = "User is not logged in"
 
 @app.route('/register', methods=['POST'])
 def register():
     if current_user.is_authenticated:
-        return jsonify([False, USER_ALREADY_LOGGED_ERROR])
+        return jsonify([False, ProtocolErrors.USER_ALREADY_LOGGED_ERROR.value])
 
     username = request.form.get('username')
     password = request.form.get('password')
     email = request.form.get('email')
     if not (username and email and password):
-        return jsonify([False, INVALID_PARAMETERS_ERROR])
+        return jsonify([False, ProtocolErrors.INVALID_PARAMETERS_ERROR.value])
 
     if validate_username(username) and validate_email(email) and validate_password(password): 
         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
         user = User(username=username, email=email, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        return jsonify([True, {}])
+        return jsonify([True, ProtocolErrors.PARAMETERS_DOES_NOT_MATCH_REQUIREMENTS.value])
 
-    return jsonify([False, {}]) #add custom error msg later
+    return jsonify([False, ProtocolErrors.PARAMETERS_DOES_NOT_MATCH_REQUIREMENTS.value]) #add custom error msg later
 
 
 @app.route('/login', methods=['POST'])
 def login():
     if current_user.is_authenticated:
-        return jsonify([False, USER_ALREADY_LOGGED_ERROR])
+        return jsonify([False, ProtocolErrors.USER_ALREADY_LOGGED_ERROR.value])
     
     auth =  request.form.get('auth')
     password = request.form.get('password')
-    
     if not (auth and password):
-        return jsonify([False, INVALID_PARAMETERS_ERROR])
+        return jsonify([False, ProtocolErrors.INVALID_PARAMETERS_ERROR.value])
 
     user = User.query.filter_by(email=auth).first() # Checking for both options (username validation or email validation)
     if not user:
@@ -49,7 +44,7 @@ def login():
         login_user(user) # Not sure how to use the remember argument
         return jsonify([True, {}])
 
-    return jsonify([False, INVALID_CREDENTIALS_ERROR])
+    return jsonify([False, ProtocolErrors.INVALID_CREDENTIALS_ERROR.value])
 
 
 @app.route('/logout')
