53a3d47032635b912c32a9c001db606258455f45
Jonathan David <jday.david.2002@gmail.com>
2019-12-05 13:47:38 +0200

    Fixed a bug concerning the user register, and added comments

--- Server/routes.py
+++ Server/routes.py
@@ -16,32 +16,36 @@ def register():
     if not (username and email and password):
         return jsonify([False, ProtocolErrors.INVALID_PARAMETERS_ERROR.value])
 
-    if validate_username(username) and validate_email(email) and validate_password(password): 
-        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    if validate_username(username) and validate_email(email) and validate_password(password):
+        hashed_password = bcrypt.generate_password_hash(
+            password).decode('utf-8')
         user = User(username=username, email=email, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        return jsonify([True, ProtocolErrors.PARAMETERS_DOES_NOT_MATCH_REQUIREMENTS.value])
+        return jsonify([True, {}])
 
-    return jsonify([False, ProtocolErrors.PARAMETERS_DOES_NOT_MATCH_REQUIREMENTS.value]) #add custom error msg later
+    # add custom error msg later
+    return jsonify([False, ProtocolErrors.PARAMETERS_DOES_NOT_MATCH_REQUIREMENTS.value])
 
 
 @app.route('/login', methods=['POST'])
 def login():
     if current_user.is_authenticated:
         return jsonify([False, ProtocolErrors.USER_ALREADY_LOGGED_ERROR.value])
-    
-    auth =  request.form.get('auth')
+
+    auth = request.form.get('auth')
     password = request.form.get('password')
     if not (auth and password):
         return jsonify([False, ProtocolErrors.INVALID_PARAMETERS_ERROR.value])
 
-    user = User.query.filter_by(email=auth).first() # Checking for both options (username validation or email validation)
+    # Checking for both options (username validation or email validation)
+    user = User.query.filter_by(email=auth).first()
     if not user:
         user = User.query.filter_by(username=auth).first()
 
     if user and bcrypt.check_password_hash(user.password, password):
-        login_user(user) # Not sure how to use the remember argument
+        # Login user using flask-login function
+        login_user(user)  # Not sure how to use the remember argument
         return jsonify([True, {}])
 
     return jsonify([False, ProtocolErrors.INVALID_CREDENTIALS_ERROR.value])
@@ -50,9 +54,10 @@ def login():
 @app.route('/logout')
 @login_required
 def logout():
+    # A flask-login function that disconnects the user
     logout_user()
     return jsonify([True, {}])
 
 
-#NOTE: how should we use the is_active method for current_user?
-#NOTE: the login process should be diffrent for API?
+# NOTE: how should we use the is_active method for current_user?
+# NOTE: the login process should be diffrent for API?
