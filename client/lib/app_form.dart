57866916480a7d34db9e5d6b5f506a198c3dc7fc
itay shirizly <it.shirizly@gmail.com>
2019-12-26 22:29:23 +0200

    Re structured the code, added password hide/show button

---
+++ client/lib/app_form.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppForm extends StatefulWidget {
+  final String hint;
+  final bool isPassword;
+
+  AppForm({Key key, @required String this.hint, bool this.isPassword = false}) : super(key: key);
+
+  @override
+  AppFormState createState() => AppFormState();
+}
+
+class AppFormState extends State<AppForm> {
+  bool _showForm = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: widget.isPassword? !_showForm : false, //only obscure the text if it password form
+      decoration: InputDecoration(
+        suffixIcon: widget.isPassword? GestureDetector( //only add the icon if it is password form
+          onTap: () {
+            setState(() {
+              _showForm = !_showForm;
+            });
+          },
+          child: Icon(_showForm ? Icons.visibility : Icons.visibility_off)) : null,
+        labelText: widget.hint,
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(4))),
+    );
+  }
+}
--- client/lib/login.dart
+++ client/lib/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:circular_check_box/circular_check_box.dart';
+import 'package:client/app_form.dart';
 
 class LoginPage extends StatefulWidget{
   @override
@@ -15,21 +16,20 @@ class LoginPageState extends State<LoginPage> {
       body:  Container(
           margin: const EdgeInsets.only(left: 25.0, right: 25.0),
           child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
+            SizedBox(height: 50),
             Image.asset('assets/voice_asistant_icon.png', scale: 3),
+            SizedBox(height: 10),
             Text('Login', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
             Text('Please login to continue using our app.', style: TextStyle(fontWeight: FontWeight.w300)),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: "Username or Email",
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(4))),
+            SizedBox(height: 20),
+            AppForm(
+              hint: "Username or Email",
             ),
-            TextFormField( //TODO: add option to show the password for a moment
-              obscureText: true, 
-              decoration: InputDecoration(
-                labelText: "Password",
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(4))),
+            SizedBox(height: 20),
+            AppForm(
+              hint: 'Password',
+              isPassword: true,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -37,6 +37,7 @@ class LoginPageState extends State<LoginPage> {
                 Row(children: <Widget>[
                   CircularCheckBox(
                     value: rememberMe,
+                    activeColor: Colors.black87,
                     onChanged: (bool value) {
                       setState(() {
                         rememberMe = value;
@@ -44,16 +45,18 @@ class LoginPageState extends State<LoginPage> {
                     ),
                     Text("Remember me")
                 ]),
-                FlatButton(child: Text('Forgot password?'),)
+                FlatButton(child: Text('Forgot password?'))
               ]),
+              SizedBox(height: 15),
               Container(
                 height: 70,
                 width: double.infinity,
-                child: FlatButton(
+                child: RaisedButton(
                         color: Colors.black87,
                         textColor: Colors.white,
-                        child: Text('Login'),
+                        child: Text('Login', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
                         onPressed: () {},
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4))
                       )
               ),
               Row(
@@ -63,7 +66,7 @@ class LoginPageState extends State<LoginPage> {
                   FlatButton(
                     child: Text('Sign Up', style: TextStyle(color: Colors.blue)),
                   )
-              ],)
+              ],),
             ],
           ),
       )
