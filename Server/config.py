c887a2832de1f9fa56dd3a7d7392dbed8d309148
itay shirizly <it.shirizly@gmail.com>
2020-02-25 13:55:34 +0200

    Changed the config file to be with classes instead of dictionaries, also added config for the nlp module

--- Server/config.py
+++ Server/config.py
@@ -1,4 +1,5 @@
 import os
+from Server.nlp import nlp_wiki, nlp_coin_exchange, nlp_translate
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
@@ -10,13 +11,33 @@ class Config:
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
 
-internet_scrappers = {
-    'SENTENCES_COUNT' : 2,
-    'EXCHANGE_API_URL' : r"https://api.exchangerate-api.com/v4/latest/"
-}
+class InternetScrappersSettings():
+    SENTENCES_COUNT = 2
+    EXCHANGE_API_URL = r"https://api.exchangerate-api.com/v4/latest/"
 
 
-calculator = {
-    'MAX_PASSWORD_LEN': 32,
-    'MIN_PASSWORD_LEN': 8
-}
+class ValidatorsSettings():
+    MAX_PASSWORD_LEN = 32
+    MIN_PASSWORD_LEN = 8
+
+
+class NLPSettings(): # Settings for the nlp module
+    command_dict = {
+    'exchange': nlp_coin_exchange,
+    'translate': nlp_translate,
+    'say': nlp_translate,
+    'tell': nlp_wiki    
+    }
+    wh_dict = {
+        'what': nlp_wiki,
+        'who': nlp_wiki,
+        'where': nlp_wiki,
+        'which': nlp_wiki,
+    }
+    how_dict = {
+        'much': nlp_coin_exchange,
+        'many': nlp_coin_exchange, # might change
+        'does': nlp_wiki,
+        'do': nlp_translate 
+    }
+    LANGUAGE_PATTERN = [{'LOWER': {'IN': ['to','in']}}, {'ENT_TYPE': {'IN': ['NORP','LANGUAGE']}}]
