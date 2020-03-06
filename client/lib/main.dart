ea0f62be1ce21845f108b4ec6aff34f6cfffc1aa
itay shirizly <it.shirizly@gmail.com>
2020-03-06 10:44:06 +0200

    Basic speech_to_text (still not working - network_error)

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:client/login.dart';
 import 'package:client/register.dart';
+import 'package:client/speech.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget{
     Widget build(BuildContext context){
       return MaterialApp(
         theme: ThemeData(primaryColor: Colors.black),
-        home: LoginPage(),
+        home: SpeechRecognitionScreen(),
         routes: {
           '/login' : (_) => LoginPage(),
           '/register': (_) => RegisterPage(),
--- client/lib/speech.dart
+++ client/lib/speech.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:client/login_system_template.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class SpeechRecognitionScreen extends StatefulWidget {
   SpeechRecognitionScreen({Key key}) : super(key: key);
@@ -9,6 +11,21 @@ class SpeechRecognitionScreen extends StatefulWidget {
 }
 
 class _SpeechRecognitionScreenState extends State<SpeechRecognitionScreen> {
+  SpeechToText _speech;  
+  bool _isAvailable = false;
+  String _text = "";
+  
+  @override
+  void initState(){
+    super.initState();
+    _initSpeechState();
+  }
+
+  Future<void> _initSpeechState() async {
+    _speech  = SpeechToText();
+    _isAvailable = await _speech.initialize();
+  }
+
   @override
   Widget build(BuildContext context) {
     return LoginSystemTemplate(
@@ -17,16 +34,39 @@ class _SpeechRecognitionScreenState extends State<SpeechRecognitionScreen> {
           height: MediaQuery.of(context).size.height * 0.8,
           padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height * 0.1),
           alignment: Alignment.topLeft,
-          child: Text('Hello world! Translate what a beautifull day to Hebrew', style: TextStyle(fontSize: 25),),
+          child: Text(_text, style: TextStyle(fontSize: 25),),
         ),
         Align(
           alignment: Alignment.bottomCenter,
-          child: FloatingActionButton(
-              onPressed:  () {},
-              child: Icon(Icons.mic),
-            )
+          child: GestureDetector(
+            child: FloatingActionButton(
+                child: Icon(Icons.mic),
+            ),
+            onLongPress: () {
+              print('listening');
+              _startListening();
+            },
+            onLongPressEnd: (LongPressEndDetails _) {
+              print('Stoped listening: $_text');
+              _stopListening();
+            },
+          )
         )
       ],
     );
+  }
+
+  _startListening() {
+    if (_isAvailable){
+      _speech.listen(onResult: (SpeechRecognitionResult result) {
+          _text = "${result.recognizedWords} - ${result.finalResult}";
+        });
+      }
+    setState(() { });
+  }
+
+  _stopListening() {
+    _speech.stop();
+    setState(() {});
   }
 }
