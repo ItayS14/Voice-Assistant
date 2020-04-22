96dc48d200cf39cb7453a87150bae5280f931eef
Jonathan David <jday.david.2002@gmail.com>
2020-04-22 17:48:31 +0300

    Removed all unnecessary models and fields from the database

--- Server/db_models.py
+++ Server/db_models.py
@@ -15,7 +15,6 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(16), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    house_id = db.Column(db.Integer, db.ForeignKey('house.id'))
     reset_code = db.Column(db.String(6), unique=True, nullable=True)
     updated_time = db.Column(db.Float, nullable=True)
     profile_image = db.Column(db.String(32), nullable=False, default='default.jpg')
@@ -50,44 +49,3 @@ class User(db.Model, UserMixin):
         
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
-
-
-class Device(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), nullable=False)
-    # Maybe need to convert this to db.ForeignKey
-    device_type_id = db.Column(db.Integer)
-    options = db.Column(db.String(100))
-    house_id = db.Column(db.Integer, db.ForeignKey('house.id'), nullable=False)
-
-    def __repr__(self):
-        return f"Device('{self.device_name}', Type: {self.device_type_id})"
-
-# Need to think how to connect an house to the server
-
-
-class House(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), nullable=False)
-    devices = db.relationship('Device', backref='house', lazy=True)
-    users = db.relationship('User', backref='house', lazy=True)
-
-    def __repr__(self):
-        return f"House('{self.name}', Devices: {self.devices}, Users: {self.users})"
-
-
-class DeviceType(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-
-    def __repr__(self):
-        return f"DeviceType({self.device_type_id})"
-
-
-class Command(db.Model):
-    index = db.Column(db.Integer, primary_key=True)
-    # All the words that can describe the command, as a list
-    command_words = db.Column(db.String(5000))
-
-    def __repr__(self):
-        return f"Command('{self.index}': {self.command_words})"
